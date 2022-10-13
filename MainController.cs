8540f1102819fcab3b01a6f2afed6d2487829a25
bat1skaf from the shkaf <108030989+bat1skaf@users.noreply.github.com>
2022-10-13 11:42:31 +0300

    Create MainController.cs

---
+++ MainController.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+// ...
+
+namespace AppSocial {
+
+		public class Login : HomeController {
+    
+    		static void Main(string[] args) {
+        		public async AddCookieIntoDB();
+        }
+    		public async AddCookieIntoDB<CookieSettings> => {
+        		if(context.Request.Cookies.ContainsKey("name")) {
+            		string name = context.Request.Cookies.ContainsKey("name")
+            }
+            
+            db.EnsureCreated();
+            db.Add(Tokens, name)
+        }
+    }
+}
